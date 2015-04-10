fa5d290c4ab1218c75ffdcb18ea9859e57376ee8
Belson <belson@server.local>
2015-04-10 11:28:55 +0800

    initial commit

--- script/start-collamine-clients.sh
+++ script/start-collamine-clients.sh
@@ -2,8 +2,8 @@
 FILE="/tmp/collamine"
 if [ -f $FILE ]; then
    echo "File '$FILE' Exists"
-   ruby ~/gems/collamine/test.rb
-   bash ~/git/collamine-client-python/scrapybot/run.sh
+   ssh cm07 ruby ~/gems/collamine/test.rb
+   ssh cm06 bash ~/git/collamine-client-python/scrapybot/run.sh
    echo "Removing '$FILE'"
    rm $FILE
 else
