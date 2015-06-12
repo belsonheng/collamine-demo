db5ac25263f4439361d4e0b0273a762174b89fe4
Belson <belson@server.local>
2015-06-12 13:47:21 +0800

    update script

--- script/start-collamine-clients.sh
+++ script/start-collamine-clients.sh
@@ -4,6 +4,7 @@ if [ -f $FILE ]; then
    echo "File '$FILE' Exists"
    echo "Removing '$FILE'"
    rm $FILE
+   echo 'y'
    screen -S ruby -d -m ssh cm07 ruby ~/gems/collamine/test.rb
    screen -S python -d -m ssh cm06 bash ~/git/collamine-client-python/scrapybot/run.sh
 else
