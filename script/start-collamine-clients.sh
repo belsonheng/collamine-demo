9ac7a40dba618c66391310a64a99dfdf1d00a5ec
Belson <belson@server.local>
2015-06-12 10:28:15 +0800

    update script

--- script/start-collamine-clients.sh
+++ script/start-collamine-clients.sh
@@ -2,10 +2,10 @@
 FILE="/tmp/collamine"
 if [ -f $FILE ]; then
    echo "File '$FILE' Exists"
-   ssh cm07 ruby ~/gems/collamine/test.rb
-   ssh cm06 bash ~/git/collamine-client-python/scrapybot/run.sh
    echo "Removing '$FILE'"
    rm $FILE
+   screen -S ruby -d -m ssh cm07 ruby ~/gems/collamine/test.rb
+   screen -S python -d -m ssh cm06 bash ~/git/collamine-client-python/scrapybot/run.sh
 else
    echo "The File '$FILE' Does Not Exist"
 fi
