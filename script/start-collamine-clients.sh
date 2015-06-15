ac2c373d8dddf27bbc57ca942b567a5ec0bea56e
Belson <belson@server.local>
2015-06-15 17:57:47 +0800

    update script to use nohup

--- script/start-collamine-clients.sh
+++ script/start-collamine-clients.sh
@@ -6,8 +6,8 @@ if [ -f $FILE ]; then
    yes | rm $FILE
    #screen -S ruby -d -m ssh cm07 ruby ~/gems/collamine/test.rb
    #screen -S python -d -m ssh cm06 bash ~/git/collamine-client-python/scrapybot/run.sh
-   nohup ssh cm07 ruby ~/gems/collamine/test.rb 2>&1 >> /tmp/ruby.log
-   nohup ssh cm06 bash ~/git/collamine-client-python/scrapybot/run.sh 2>&1 >> /tmp/python.log
+   nohup ssh cm07 && ruby ~/gems/collamine/test.rb 2>&1 >> /tmp/ruby.log
+   nohup ssh cm06 && bash ~/git/collamine-client-python/scrapybot/run.sh 2>&1 >> /tmp/python.log
 else
    echo "The File '$FILE' Does Not Exist"
 fi
