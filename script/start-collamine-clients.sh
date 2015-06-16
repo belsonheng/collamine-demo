dc252b18a0fce2ec73fd7ecc9db9da94f6744906
Belson <belson@server.local>
2015-06-16 12:19:59 +0800

    update script to use nohup

--- script/start-collamine-clients.sh
+++ script/start-collamine-clients.sh
@@ -7,7 +7,7 @@ if [ -f $FILE ]; then
    echo "Start Ruby Crawler"
    #screen -S ruby -d -m ruby ~/gems/collamine/test.rb
    echo "Start Python Crawler"
-   ssh cm06 'screen -S python -d -m ./home/belson/git/collamine-client-python/scrapybot/run.sh'
+   ssh cm06 "nohup bash ~/git/collamine-client-python/scrapybot/run.sh > /dev/null 2>&1 >> /tmp/python.log &"
    #nohup ssh cm07 && ruby ~/gems/collamine/test.rb 2>&1 >> /tmp/ruby.log
    #nohup ssh cm06 && bash ~/git/collamine-client-python/scrapybot/run.sh 2>&1 >> /tmp/python.log
 else
