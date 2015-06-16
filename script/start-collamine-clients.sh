706f412b64a240d7676dd0d84b02a861b61ac0dd
Belson <belson@server.local>
2015-06-16 14:27:14 +0800

    update script to use screen

--- script/start-collamine-clients.sh
+++ script/start-collamine-clients.sh
@@ -5,9 +5,9 @@ if [ -f $FILE ]; then
    echo "Removing '$FILE'"
    yes | rm $FILE
    echo "Start Ruby Crawler"
-   screen -S ruby -d -m ./ruby.sh
+   screen -S ruby -d -m ruby ~/gems/collamine/test.rb
    echo "Start Python Crawler"
-   screen -S python -d -m ./python.sh
+   screen -S python -d -m 'bash -s' < python.sh
    #nohup ssh cm07 && ruby ~/gems/collamine/test.rb 2>&1 >> /tmp/ruby.log
    #nohup ssh cm06 && bash ~/git/collamine-client-python/scrapybot/run.sh 2>&1 >> /tmp/python.log
 else
