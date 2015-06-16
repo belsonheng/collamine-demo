0885d78fefcbc88edb8346d7c561b4902245f0b2
Belson <belson@server.local>
2015-06-16 11:27:19 +0800

    update script to use screen

--- script/start-collamine-clients.sh
+++ script/start-collamine-clients.sh
@@ -7,7 +7,7 @@ if [ -f $FILE ]; then
    echo "Start Ruby Crawler"
    screen -S ruby -d -m ssh cm07 ruby ~/gems/collamine/test.rb
    echo "Start Python Crawler"
-   ssh cm06 screen -S python -d -m 'bash -s' < ~/git/collamine-client-python/scrapybot/run.sh
+   ssh cm06 screen -S python -d -m 'bash -s ~/git/collamine-client-python/scrapybot/run.sh'
    #nohup ssh cm07 && ruby ~/gems/collamine/test.rb 2>&1 >> /tmp/ruby.log
    #nohup ssh cm06 && bash ~/git/collamine-client-python/scrapybot/run.sh 2>&1 >> /tmp/python.log
 else
